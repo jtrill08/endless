af449559e123991420a97b0f1fbf2e49213df359
jtrill08 <49694135+jtrill08@users.noreply.github.com>
2023-12-07 08:28:09 +0000

    Update cleveland.js

--- cleveland.js
+++ cleveland.js
@@ -31,38 +31,46 @@ function getRandomArtworksCleveland() {
 
 function createArtworkElement(artwork) {
     const artworkInfo = document.createElement('div');
-    artworkInfo.classList.add('artwork-info');
-
-    const titleElement = document.createElement('p');
-    titleElement.innerHTML = `<strong>Title:</strong> ${artwork.title}`;
-    artworkInfo.appendChild(titleElement);
-
-    const artistElement = document.createElement('p');
-    artistElement.innerHTML = `<strong>Artist:</strong> ${(artwork.creators && artwork.creators.length > 0) ? artwork.creators[0].description : 'Unknown Artist'}`;
-    artworkInfo.appendChild(artistElement);
-
-    const dateElement = document.createElement('p');
-    dateElement.innerHTML = `<strong>Date:</strong> ${artwork.creation_date}`;
-    artworkInfo.appendChild(dateElement);
-
-    const mediumElement = document.createElement('p');
-    mediumElement.innerHTML = `<strong>Medium:</strong> ${artwork.type}`;
-    artworkInfo.appendChild(mediumElement);
-
-    const descriptionElement = document.createElement('p');
-    descriptionElement.innerHTML = `<strong>Description:</strong> ${artwork.description}`;
-    artworkInfo.appendChild(descriptionElement);
-
-    const didyouknowElement = document.createElement('p');
-    didyouknowElement.innerHTML = `<strong>Did you know:</strong> ${artwork.did_you_know}`;
-    artworkInfo.appendChild(didyouknowElement);
+    artworkInfo.classList.add('art-card');
 
     if (artwork.images && artwork.images.web) {
         const imageElement = document.createElement('img');
         imageElement.src = artwork.images.web.url;
         imageElement.alt = 'Artwork';
+        imageElement.className = 'artImage';
         artworkInfo.appendChild(imageElement);
     }
+
+    const artInfoContainer = document.createElement('div');
+    artInfoContainer.classList.add('artInfo');
+
+    const artistElement = document.createElement('p');
+    artistElement.className = 'artistName'; // Set the class here
+    artistElement.innerHTML = `<strong>Artist:</strong> ${(artwork.creators && artwork.creators.length > 0) ? artwork.creators[0].description : 'Unknown Artist'}`;
+    artworkInfo.appendChild(artistElement);
+
+    const titleElement = document.createElement('p');
+    titleElement.className = 'artTitle'; // Set the class here
+    titleElement.innerHTML = `<strong>Title:</strong> ${artwork.title}`;
+    artworkInfo.appendChild(titleElement);
+
+
+    const dateElement = document.createElement('p');
+    titleElement.className = 'artDate'; // Set the class here
+    dateElement.innerHTML = `<strong>Date:</strong> ${artwork.creation_date}`;
+    artworkInfo.appendChild(dateElement);
+
+    const mediumElement = document.createElement('p');
+    titleElement.className = 'artMedium'; // Set the class here
+    mediumElement.innerHTML = `<strong>Medium:</strong> ${artwork.type}`;
+    artworkInfo.appendChild(mediumElement);
+
+    const descriptionElement = document.createElement('p');
+    titleElement.className = 'artDescription'; // Set the class here
+    descriptionElement.innerHTML = `<strong>Description:</strong> ${artwork.description}`;
+    artworkInfo.appendChild(descriptionElement);
+
+
 
     return artworkInfo;
 }
@@ -70,14 +78,21 @@ function createArtworkElement(artwork) {
 // Cleveland.js
 const clevelandTab = document.getElementById('clevelandTab');
 
+let isFetching = false;
+
 clevelandTab.addEventListener('scroll', () => {
-    // Calculate the scroll percentage
     const scrollPercentage = (clevelandTab.scrollTop + clevelandTab.clientHeight) / clevelandTab.scrollHeight * 100;
 
-    // Check if the user has scrolled to 80% from the bottom of the Cleveland tab content
-    if (scrollPercentage >= 80) {
+    if (scrollPercentage >= 80 && !isFetching) {
+        isFetching = true;
+
         // Load more artworks for Cleveland
         console.log('Fetched from Cleveland');
         getRandomArtworksCleveland();
+
+        // Allow the next request after a certain delay (e.g., 2 seconds)
+        setTimeout(() => {
+            isFetching = false;
+        }, 2000);
     }
 });
