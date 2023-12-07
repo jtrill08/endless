867dd65c143cf44025e928036f343273a9b5a16a
jtrill08 <49694135+jtrill08@users.noreply.github.com>
2023-12-07 08:27:51 +0000

    Update chicago.js

--- chicago.js
+++ chicago.js
@@ -4,11 +4,18 @@ const iiifBaseUrl = 'https://www.artic.edu/iiif/2';
 
 const artworkContainer = document.getElementById('artworkContainer');
 let displayedArtworkIds = [];
+let isLoading = false; // Flag to track whether a request is in progress
 
 // Call the function to get and display artworks on page load
 getRandomArtworks();
 
 function getRandomArtworks() {
+    if (isLoading) {
+        return; // If a request is in progress, do not initiate another one
+    }
+
+    isLoading = true; // Set the flag to true as a request is now in progress
+
     // Generate a random page number
     const randomPage = Math.floor(Math.random() * 10295) + 1;
 
@@ -33,18 +40,54 @@ function getRandomArtworks() {
         })
         .catch(error => {
             console.error('Error:', error);
+            console.error('API may be overloaded or there is an issue with the request.');
+        })
+        .finally(() => {
+            isLoading = false; // Reset the flag when the request is completed, whether successful or not
         });
 }
 
 function displayArtwork(artwork) {
-    const artworkDiv = document.createElement('div');
-    artworkDiv.innerHTML = `
-        <img src="${iiifBaseUrl}/${artwork.image_id}/full/843,/0/default.jpg" alt="Artwork">
-        <p>Title: ${artwork.title}</p>
-        <p>Artist: ${artwork.artist_display}</p>
+    const artCardDiv = document.createElement('div');
+    artCardDiv.classList.add('art-card'); // Add a class for styling if needed
+
+    const imageElement = document.createElement('img');
+    imageElement.classList.add('artImage');
+
+    // Attempt to load the default image size
+    imageElement.src = `${iiifBaseUrl}/${artwork.image_id}/full/843,/0/default.jpg`;
+
+    // Add an event listener to handle errors
+    imageElement.addEventListener('error', () => {
+        // If an error occurs, try alternative image sizes
+        const alternativeSizes = [200, 400, 600];
+        for (const size of alternativeSizes) {
+            const alternativeSrc = `${iiifBaseUrl}/${artwork.image_id}/full/${size},/0/default.jpg`;
+            // Check if the alternative image size is available
+            fetch(alternativeSrc)
+                .then(response => {
+                    if (response.ok) {
+                        imageElement.src = alternativeSrc;
+                    }
+                })
+                .catch(error => {
+                    console.error(`Error fetching alternative image: ${error}`);
+                });
+        }
+    });
+
+    artCardDiv.appendChild(imageElement);
+    artCardDiv.innerHTML += `
+    <div class="artInfo">
+        <p class="artistName">Artist: ${artwork.artist_display}</p>
+        <p class="artTitle">Title: ${artwork.title}</p>
+        <p class="artMedium">Medium: ${artwork.medium_display}</p>
+    </div>
     `;
-    artworkContainer.appendChild(artworkDiv);
+
+    artworkContainer.appendChild(artCardDiv);
 }
+
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -57,15 +100,18 @@ function shuffleArray(array) {
 // Chicago.js
 const chicagoTab = document.getElementById('chicagoTab');
 
+let scrollTimeout;
 chicagoTab.addEventListener('scroll', () => {
-    // Calculate the scroll percentage
-    const scrollPercentage = (chicagoTab.scrollTop + chicagoTab.clientHeight) / chicagoTab.scrollHeight * 100;
+    // Clear the previous timeout
+    clearTimeout(scrollTimeout);
 
-    // Check if the user has scrolled to 80% from the bottom of the Chicago tab content
-    if (scrollPercentage >= 80) {
-        // Load more artworks for Chicago
-        console.log('Fetched from Chicago');
-        getRandomArtworks();
-    }
+    // Set a new timeout to debounce the scroll event
+    scrollTimeout = setTimeout(() => {
+        // Check if the user has reached the bottom of the Chicago tab content
+        if (chicagoTab.scrollHeight - chicagoTab.scrollTop === chicagoTab.clientHeight) {
+            // Load more artworks for Chicago
+            console.log('Fetched from Chicago');
+            getRandomArtworks();
+        }
+    }, 200); // Adjust the debounce time as needed
 });
-
